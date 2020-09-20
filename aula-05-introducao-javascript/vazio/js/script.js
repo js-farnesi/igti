4362c69ad9ad25d7a6bd7cc3717b7e1566985e63
js-farnesi <farnesipereira@yahoo.com.br>
2020-09-20 11:15:55 -0300

    feat: manipulando o html com o JS

---
+++ aula-05-introducao-javascript/vazio/js/script.js
@@ -0,0 +1,11 @@
+console.log('Hello World');
+
+var title = document.querySelector('h1');
+title.textContent = 'JavaScritp manipula o HTML';
+
+// estamos munipulando o elemento h1 do HTML
+
+/* Para usar comentário de código no VSCODE podemos usar atalhos
+CTRL + K + C ou CTRL + K + U, os mesmo podem ser usados para descomentar
+CTRL + ;
+*/
