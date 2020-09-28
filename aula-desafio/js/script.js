94d32d3ea212a127364cdce104ba7237d97b088a
js-farnesi <farnesipereira@yahoo.com.br>
2020-09-28 02:47:47 -0300

    feat: aula desafio programa para selecionar RGB

--- aula-desafio/js/script.js
+++ aula-desafio/js/script.js
@@ -1,7 +1,43 @@
-var
+//window.addEventListener('load', start);
 
+var inputRed = null;
+var inputGreen = null;
+var inputBlue = null;
+var rangeRed = null;
+var rangeGreen = null;
+var rangeBlue = null;
+var divSquare = null;
 
+function start() {
+  rangeRed = document.querySelector('#range-red');
+  rangeGreen = document.querySelector('#range-green');
+  rangeBlue = document.querySelector('#range-blue');
 
-var red = 
+  inputRed = document.querySelector('#inputRed');
+  inputGreen = document.querySelector('#inputGreen');
+  inputBlue = document.querySelector('#inputBlue');
 
-var rgbCSS = 'rgb('
+  divSquare = document.querySelector('#square');
+
+  rangeRed.addEventListener('input', setColor);
+  rangeGreen.addEventListener('input', setColor);
+  rangeBlue.addEventListener('input', setColor);
+
+  setColor();
+}
+
+function setColor() {
+  var red = parseInt(rangeRed.value, 10);
+  var green = parseInt(rangeGreen.value, 10);
+  var blue = parseInt(rangeBlue.value, 10);
+
+  inputRed.value = red;
+  inputGreen.value = green;
+  inputBlue.value = blue;
+
+  var rgbCSS = 'rgb(' + red + ',' + green + ',' + blue + ')';
+
+  divSquare.style.backgroundColor = rgbCSS;
+}
+
+start();
