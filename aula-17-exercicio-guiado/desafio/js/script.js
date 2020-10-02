b84c4702dc524d4564e8150cc2c7574e546e03ed
js-farnesi <farnesipereira@yahoo.com.br>
2020-10-01 21:27:58 -0300

    chore: preparando CSS HTML e variáveis JS para contador da população de países usando API do restcountries

---
+++ aula-17-exercicio-guiado/desafio/js/script.js
@@ -0,0 +1,32 @@
+// Estado da aplicação state
+let tabCountries = null;
+let tabFavorites = null;
+
+let allCountries = [];
+let favoriteCountries = [];
+
+let countContries = 0;
+let countFavorites = 0;
+
+let totalPopulationList = 0;
+let totalPopulationFavorites = 0;
+
+let numberFormat = null;
+window.addEventListener('load', () => {
+  tabCountries = document.querySelector('#tabCountries');
+  tabFavorites = document.querySelector('#tabFavorites');
+  countCountries = document.querySelector('#countCountries');
+  countFavorites = document.querySelector('#countFavorites');
+
+  totalPopulationList = document.querySelector('#totalPopulationList');
+  //prettier-ignore
+  totalPopulationFavorites = 
+  document.querySelector('#totalPopulationFavorites');
+
+  numberFormat = Intl.NumberFormat('pt-BR');
+
+  fetchCountries();
+});
+function fetchCountries() {
+  console.log('fetting ...');
+}
