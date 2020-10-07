45b5a6d8160efefda4b4f3d2b3ea1db7b538959a
js-farnesi <farnesipereira@yahoo.com.br>
2020-10-06 22:18:08 -0300

    feat: usando launch.json a passando args

---
+++ aula-2.01-nodejs/index.js
@@ -0,0 +1,10 @@
+const numero = parseInt(process.argv[2]);
+const multiplos = [];
+
+for (let i = 0; i < numero; i++) {
+  if (i % 3 === 0 || i % 5 === 0) {
+    multiplos.push(i);
+  }
+}
+
+console.log(multiplos);
