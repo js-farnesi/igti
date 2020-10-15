f5b7f0577a4ea1ac7712333948682c649f590564
js-farnesi <farnesipereira@yahoo.com.br>
2020-10-14 21:47:50 -0300

    feat: rotas para PUT PATCH

--- MY-BANK-API/routes/accounts.js
+++ MY-BANK-API/routes/accounts.js
@@ -39,4 +39,58 @@ router.get('/', async (_req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  try {
+    const data = JSON.parse(await readFile(global.fileName));
+    const account = data.accounts.find(
+      (account) => account.id === parseInt(req.params.id)
+    );
+    res.send(account);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const data = JSON.parse(await readFile(global.fileName));
+    data.accounts = data.accounts.filter(
+      (account) => account.id !== parseInt(req.params.id)
+    );
+    await writeFile(global.fileName, JSON.stringify(data, null, 2));
+    res.send(`ID ${req.params.id} deletado`);
+    res.end();
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
+router.put('/', async (req, res) => {
+  try {
+    let account = req.body;
+    const data = JSON.parse(await readFile(global.fileName));
+    const index = data.accounts.findIndex((a) => a.id === account.id);
+    data.accounts[index] = account;
+
+    await writeFile(global.fileName, JSON.stringify(data, null, 2));
+    res.send(account);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
+router.patch('/updateBalance', async (req, res) => {
+  try {
+    let account = req.body;
+    const data = JSON.parse(await readFile(global.fileName));
+    const index = data.accounts.findIndex((a) => a.id === account.id);
+    data.accounts[index].balance = account.balance;
+
+    await writeFile(global.fileName, JSON.stringify(data, null, 2));
+    res.send(data.accounts[index]);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
 export default router;
