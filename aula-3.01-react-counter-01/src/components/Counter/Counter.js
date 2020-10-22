042c03fd72d23f1fc64b331ba60a482f7e9e58d0
js-farnesi <farnesipereira@yahoo.com.br>
2020-10-21 21:33:06 -0300

    feat: interação com o button down e up alterando pelo setState

--- aula-3.01-react-counter-01/src/components/Counter/Counter.js
+++ aula-3.01-react-counter-01/src/components/Counter/Counter.js
@@ -5,29 +5,48 @@ export default class Counter extends Component {
   constructor() {
     super();
 
-    this.currentCounter = 2;
+    this.state = {
+      currentCounter: 2,
+      steps: 0,
+    };
   }
 
-  handleClick = () => {
-    console.log('click');
-    //não reflete a alteração de valor, nem chamando o render() porque o atributo this.currentCounter = 2; é inútil para o React que trabalha com o state
-    this.currentCounter--;
-    this.render();
+  handleButtonDownClick = () => {
+    const { currentCounter, steps } = this.state;
+
+    this.setState({
+      currentCounter: currentCounter - 1,
+      steps: steps + 1,
+    });
+  };
+
+  handleButtonUpClick = () => {
+    const { currentCounter, steps } = this.state;
+
+    this.setState({
+      currentCounter: currentCounter + 1,
+      steps: steps + 1,
+    });
   };
 
   render() {
+    const { currentCounter, steps } = this.state;
     return (
       <div className={css.counterContainer}>
         <button
-          onClick={this.handleClick}
+          onClick={this.handleButtonDownClick}
           className="waves-effect waves-light btn red darken-4"
+        >
+          -
+        </button>
+        <span className={css.counterValue}>{currentCounter}</span>
+        <button
+          onClick={this.handleButtonUpClick}
+          className="waves-effect waves-light btn green darken-4"
         >
           +
         </button>
-        <span className={css.counterValue}>{this.currentCounter}</span>
-        <button className="waves-effect waves-light btn green darken-4">
-          -
-        </button>
+        <span className={css.counterValue}>({steps})</span>
       </div>
     );
   }
