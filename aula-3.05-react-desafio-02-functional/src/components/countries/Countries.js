a4c91e337d158fa285e0a9f69fe915207489c556
js-farnesi <farnesipereira@yahoo.com.br>
2020-10-27 00:09:16 -0300

    feat: migração do class component para functional compoment no desafio 2

--- aula-3.05-react-desafio-02-functional/src/components/countries/Countries.js
+++ aula-3.05-react-desafio-02-functional/src/components/countries/Countries.js
@@ -1,18 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Country from './Country';
 
 import css from './countries.module.css';
 
-export default class Countries extends Component {
-  render() {
-    const { countries } = this.props;
-
-    return (
-      <div className={`${css.border} ${css.flexRow}`}>
-        {countries.map((country) => {
-          return <Country key={country.id} country={country} />;
-        })}
-      </div>
-    );
-  }
+export default function Countries({ countries }) {
+  return (
+    <div className={`${css.border} ${css.flexRow}`}>
+      {countries.map((country) => {
+        return <Country key={country.id} country={country} />;
+      })}
+    </div>
+  );
 }
