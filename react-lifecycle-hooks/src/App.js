c3858ecd56004650a87290d40fabbc8b575b385d
js-farnesi <farnesipereira@yahoo.com.br>
2020-10-31 22:13:15 -0300

    fix: lifecycle com hooks

--- react-lifecycle-hooks/src/App.js
+++ react-lifecycle-hooks/src/App.js
@@ -1,59 +1,70 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Toggle from './components/Toggle/Toggle';
 import Items from './components/User/Items';
 
-export default class App extends Component {
-  constructor() {
-    super();
+export default function App() {
+  const [items, setItems] = useState([]);
+  const [showData, setShowData] = useState(false);
+  // constructor() {
+  //   super();
 
-    this.state = {
-      showData: false,
-      items: [],
+  //   this.state = {
+  //     showData: false,
+  //     items: [],
+  //   };
+
+  //   this.interval = null;
+  // }
+
+  // this.setState({
+  //   showData: checked,
+  // });
+
+  useEffect(() => {
+    const fecthUsers = async () => {
+      const res = await fetch(
+        'https://randomuser.me/api/?seed=rush&nat=br&results=10'
+      );
+      const json = await res.json();
+      setItems(json.results);
     };
 
-    this.interval = null;
-  }
+    fecthUsers();
+  }, []);
 
-  handleToggle = (checked) => {
-    this.setState({
-      showData: checked,
-    });
+  const handleToggle = (ischecked) => {
+    setShowData(ischecked);
+    // async componentDidMount() {
+    //   const res = await fetch(
+    //     'https://randomuser.me/api/?seed=rush&nat=br&results=10'
+    //   );
+    //   const json = await res.json();
+
+    //   const items = json.results.map((item) => {
+    //     const { name, picture, login } = item;
+
+    //     return {
+    //       id: login.uuid,
+    //       name: name.first,
+    //       picture: picture.large,
+    //     };
+    //   });
+
+    //   setTimeout(() => {
+    //     this.setState({ items });
+    //   }, 5000);
+    // }
   };
 
-  async componentDidMount() {
-    const res = await fetch(
-      'https://randomuser.me/api/?seed=rush&nat=br&results=10'
-    );
-    const json = await res.json();
+  return (
+    <div style={{ padding: '10px' }}>
+      <h1>Abra o console!</h1>
 
-    const items = json.results.map((item) => {
-      const { name, picture, login } = item;
-
-      return {
-        id: login.uuid,
-        name: name.first,
-        picture: picture.large,
-      };
-    });
-
-    setTimeout(() => {
-      this.setState({ items });
-    }, 5000);
-  }
-
-  render() {
-    const { showData, items } = this.state;
-
-    return (
-      <div style={{ padding: '10px' }}>
-        <h1>Abra o console!</h1>
-
-        <div>
-          <Toggle description="Exibir lista" onToggle={this.handleToggle} />
-        </div>
-
-        {showData && <Items values={items} />}
+      <div>
+        <Toggle description="Exibir lista" onToggle={handleToggle} />
       </div>
-    );
-  }
+
+      {showData && <Items values={items} />}
+    </div>
+  );
 }
--- react-lifecycle-hooks/src/components/User/Items.js
+++ react-lifecycle-hooks/src/components/User/Items.js
@@ -1,58 +1,66 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Item from './Item';
 
 import css from './items.module.css';
 
-export default class Items extends Component {
-  constructor() {
-    super();
+export default function Items({ values }) {
+  const [seconds, setSeconds] = useState(0);
 
-    this.state = {
-      seconds: 0,
-    };
-
-    this.interval = null;
-  }
-
-  componentDidMount() {
-    console.log('componentDidMount');
-
-    this.interval = setInterval(() => {
-      const { seconds } = this.state;
-      this.setState({ seconds: seconds + 1 });
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds(seconds + 1);
     }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [seconds]);
+  // constructor() {
+  //   super();
+
+  //   this.state = {
+  //     seconds: 0,
+  //   };
+
+  //   this.interval = null;
+  // }
+
+  // componentDidMount() {
+  //   console.log('componentDidMount');
+
+  //   this.interval = setInterval(() => {
+  //     const { seconds } = this.state;
+  //     this.setState({ seconds: seconds + 1 });
+  //   }, 1000);
+  // }
+
+  // componentDidUpdate() {
+  //   console.log('componentDidUpdate');
+  // }
+
+  // componentWillUnmount() {
+  //   console.log('componentWillUnmount');
+  //   clearInterval(this.interval);
+  // }
+
+  // const { values } = this.props;
+  // const { seconds } = this.state;
+
+  if (!values || values.length === 0) {
+    return <p>Carregando...</p>;
   }
 
-  componentDidUpdate() {
-    console.log('componentDidUpdate');
-  }
+  return (
+    <div>
+      <p style={{ marginTop: '10px' }}>
+        Lista aberta por {seconds} segundos...
+      </p>
 
-  componentWillUnmount() {
-    console.log('componentWillUnmount');
-    clearInterval(this.interval);
-  }
-
-  render() {
-    const { values } = this.props;
-    const { seconds } = this.state;
-
-    if (!values || values.length === 0) {
-      return <p>Carregando...</p>;
-    }
-
-    return (
-      <div>
-        <p style={{ marginTop: '10px' }}>
-          Lista aberta por {seconds} segundos...
-        </p>
-
-        <div className={css.countryFlex}>
-          {values.map((item) => {
-            const { id } = item;
-            return <Item key={id} item={item} />;
-          })}
-        </div>
+      <div className={css.countryFlex}>
+        {values.map((item) => {
+          const { id } = item;
+          return <Item key={id} item={item} />;
+        })}
       </div>
-    );
-  }
+    </div>
+  );
 }
